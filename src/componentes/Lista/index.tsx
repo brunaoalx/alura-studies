4e0e1b4f7ae9f69f3297c4add486d0da28e7efc6
Bruno Alexandre <bruno.alexandre@mfmti.com.br>
2022-07-07 17:17:50 -0300

    atividades iniciais da primeira aula

--- src/componentes/Lista/index.tsx
+++ src/componentes/Lista/index.tsx
@@ -2,21 +2,23 @@ import React from "react";
 
 function Lista(){
 
-    return(
-        <aside>
-            <h2>Estudos do dia</h2>
-            <ul>
-                <li>
-                    <h3>React</h3>
-                    <span>02:00:00</span>
-                </li>
-                <li>
-                    <h3>Javascript</h3>
-                    <span>01:00:00</span>
-                </li>
-            </ul>
+   const tarefas = [
+       {tarefa: 'React', tempo:'02:00:00'}
+       ,{tarefa: 'Javascript', tempo:'01:30:00'}
+       ,{tarefa: 'Typescript', tempo:'01:00:00'}
+   ]; 
 
-        </aside>
+
+    return(
+        <div>
+            {tarefas.map((item,index) => (
+                    <div key={'tarefa' + index}>
+                        <h3>{item.tarefa}</h3>
+                        <p>{item.tempo}</p>
+                    </div>
+            )) 
+            }
+        </div>
     )
 
 }
---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Formulario from '../componentes/Formulario';
+import Lista from '../componentes/Lista';
+import './style.scss';
+
+function App() {
+  return (
+    <div className="AppStyle">
+      <Formulario/>
+      <Lista/>
+    </div>
+  );
+}
+
+export default App;
